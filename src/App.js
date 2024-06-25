99b6a3bc0964d3fd7ebac51518245e3c6825ae2b
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-25 17:22:58 +0200

    Not using bootstrap because I can't change background color otherwist

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
 import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <header className="App-header"></header>
-        <main>
-          <Dictionary />{" "}
-        </main>
-        <footer>
-          Coded by{" "}
-          <a
-            href="https://github.com/RRachelvd"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Rachèl van Dijkhorst
-          </a>
-          , open-sourced on{" "}
-          <a
-            href="https://github.com/RRachelvd/dictionary-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github{" "}
-          </a>
-          and hosted on{" "}
-          <a
-            href="https://dictionary-inspiration.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
-        </footer>
-      </div>
+      <header className="App-header"></header>
+      <main>
+        <Dictionary />{" "}
+      </main>
+      <footer>
+        Coded by{" "}
+        <a href="https://github.com/RRachelvd" target="_blank" rel="noreferrer">
+          Rachèl van Dijkhorst
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/RRachelvd/dictionary-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github{" "}
+        </a>
+        and hosted on{" "}
+        <a
+          href="https://dictionary-inspiration.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
