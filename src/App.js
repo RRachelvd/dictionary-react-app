d546d9a1b655a3fa5350928b7d6c9e46f51136e8
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-25 15:57:48 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,32 @@ function App() {
         <main>
           <Dictionary />{" "}
         </main>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/RRachelvd"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rachèl van Dijkhorst
+          </a>
+          , open-sourced on{" "}
+          <a
+            href="https://github.com/RRachelvd/dictionary-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github{" "}
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://dictionary-inspiration.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
