3f0f5558b59764f023f71fee170b09e0a131a441
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-06-25 14:51:15 +0200

    Made succesfull api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,20 +4,23 @@ import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
-  function search() {
-    alert(`Searching for the definition of ${keyword}...`);
-  }
-
-  function showDefinition() {}
-
-  let apiKey = "0c98c0be68f4tba31fe26f898obb603d";
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(showDefinition);
 
   function handleKeywordChange(event) {
     console.log(event.target.value);
     setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = "0c98c0be68f4tba31fe26f898obb603d";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data);
   }
   return (
     <div className="Dictionary">
